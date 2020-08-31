9df69059091ba815de9fde517804e1d919591aa8
Wayne Van Son <waynevanson@gmail.com>
2020-08-31 19:53:23 +0000

    add tsdocs @since directive

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,17 @@
+/**
+ * @since 0.6.8
+ */
 import { Char } from './char'
 import { ParseResult } from './ParseResult'
 import { stream } from './Stream'
 import { Parser } from './Parser'
 
+/**
+ * @summary
+ * A testing utility that runs a parser after putting the string inside a stream.
+ *
+ * @since 0.6.8
+ */
 export function run<A>(p: Parser<Char, A>, s: string): ParseResult<Char, A> {
   return p(stream(s.split('')))
 }
